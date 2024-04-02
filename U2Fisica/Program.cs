1336aa3052e51e3e23130cea5a2d916fcc9cb2ff
danie <danie@DANIELLPEZ>
2024-04-01 19:47:45 -0600

    Add project files.

---
+++ U2Fisica/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double distance, time;
+
+        Console.WriteLine("Velocity Calculator");
+        Console.WriteLine("");
+
+        Console.Write("Enter distance (meters): ");
+        distance = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter time (seconds): ");
+        time = Convert.ToDouble(Console.ReadLine());
+
+        double velocity = distance / time;
+        Console.WriteLine($"The velocity is {velocity} m/s.");
+    }
+}
+
+
+
